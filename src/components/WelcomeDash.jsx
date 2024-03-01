165a9bf0bf96bdd0f05e7b75e443de13a17f5452
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-03-01 02:11:14 +0000

    secondary commit

--- src/components/WelcomeDash.jsx
+++ src/components/WelcomeDash.jsx
@@ -1,18 +1,25 @@
-import hub from '../assets/hub.png'
+import hub from "../assets/hub.png";
+import log from "../assets/log.png";
 
 const WelcomeDash = () => {
   return (
     <div className="welcomeDash">
-        <h2 className={''}>FlexiPay</h2>
-        <img src={hub} alt="an image here" />
-
-        <div className="welcomeText">
-
-        <h2>Welcome!</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum neque repudiandae voluptatibus atque quisquam! Sit, aliquam ipsa cumque blanditiis non illo, autem voluptatum quos doloremque quae asperiores ut id veniam!</p>
-        </div>
+      <h2 className={""}>FlexiPay</h2>
+      <div className="image">
+        <img src={log} alt="an image here" />
       </div>
-  )
-}
 
-export default WelcomeDash
+      <div className="welcomeText">
+        <h2>Welcome!</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum neque
+          repudiandae voluptatibus atque quisquam! Sit, aliquam ipsa cumque
+          blanditiis non illo, autem voluptatum quos doloremque quae asperiores
+          ut id veniam!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomeDash;
